0323bd0ed27e52beca6f27fcbeb11aa6f12fa3b1
Relativiteit <alejocain@gmail.com>
2023-08-17 18:23:29 +0200

    added type hints

--- launchassistant/cli_tool.py
+++ launchassistant/cli_tool.py
@@ -1,24 +1,28 @@
 import click 
+import xml.etree.ElementTree as ET
+from typing import Dict
 
 @click.group()
-def cli():
+def cli() -> None:
     """ROS2 Launch File Parameter Tool"""
     pass
 
 @click.command()
 @click.argument('filepath', type=click.Path(exists=True))
-def list_params(filepath):
+def list_params(filepath: str) -> None:
     """List all parameters in a given launch file."""
-    click.secho("Parameters for (): ...".format(filepath), fg="blue")
+    params = parse_launch_file(filepath)
+    for name, value in params.items():
+        click.secho("{}: {}".format(name, value), fg="blue")
 
 
 @click.command()
 @click.argument('param_name')
 @click.argument('filepath', type=click.Path(exists=True))
-def validate_param(param_name, filepath):
+def validate_param(param_name: str, filepath: str) -> None:
     """Validate if a parameter exists in a the launch file"""
     #  validation for parameters
-    if param_name in parase_file_for_parameters(filepath):
+    if param_name in parse_file_for_parameters(filepath):
         click.secho("Valid parameter!", fg='green')
     else:
         click.secho("Invalid parameter!", fg="red")
@@ -29,15 +33,14 @@ cli.add_command(validate_param)
 
 
 ### Helper Functions ###
-import xml.etree.ElementTree as ET
 
-def parse_launch_file(filepath):
+def parse_launch_file(filepath: str) -> Dict[str, str]:
     """Parse ROS2 launch file to exxtract parameters."""
     tree = ET.parse(filepath)
     root = tree.getroot()
 
     # Extract parameters
-    params = {}
+    params = Dict[str, str] = {}
     for param_tag in root.findall('.//param'):
         name = param_tag.get('name')
         value = param_tag.get('value', None)
@@ -46,8 +49,6 @@ def parse_launch_file(filepath):
         params[name] = value
     
     return params
-
-
 
 
 # if implemented in a large project use setuptools !!
